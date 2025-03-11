c4ffc865c7f9e349485bd1be7a41602641712ee1
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-11 18:12:33 -0400

    Update for scenes + fixed quit method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from scripts.engine.engine_scene_manager import EngineSceneManager
 from scripts.engine.engine_display import EngineDisplay
 import pygame
 import sys
@@ -8,23 +9,20 @@ class Engine:
     def __init__(self):
         pygame.init()
         self.display = EngineDisplay(self)
+        self.scene_manager = EngineSceneManager(self)
 
     # Run the game.
-    def run(self):
+    def run(self) -> None:
         while True:
-            # Temp code that should be inside each scene.
-            # I suppose I could make an input singleton? But singletons are weird.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
+            events = pygame.event.get()
+            self.scene_manager.current_scene.update(events, self.display.display, False, self.display.delta_time)
             self.display.update()
 
     # Static because it should be callable wherever?
     @staticmethod
-    def quit():
-        pass
+    def quit() -> None:
+        pygame.quit()
+        sys.exit()
 
 
 # Create the engine and run it.

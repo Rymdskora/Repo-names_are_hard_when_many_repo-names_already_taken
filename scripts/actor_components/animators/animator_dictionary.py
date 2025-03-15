553a6a38260f657150912730d62488bfa9f528f7
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-14 23:48:00 -0400

    Initial animators

---
+++ scripts/actor_components/animators/animator_dictionary.py
@@ -0,0 +1,49 @@
+from scripts.actor_components.animators.animator_interface import AnimatorInterface
+from scripts.actor_components.component_base import ComponentBase
+from typing import Dict, List
+import pygame
+
+
+#
+class AnimatorDict(ComponentBase, AnimatorInterface):
+
+    #
+    def __init__(self, sprites: Dict[str, List[pygame.Surface]], state='idle'):
+        super().__init__()
+        self.sprite_dictionary = sprites
+        self.state = state
+        self.num_sprites = len(self.sprite_dictionary[self.state])
+        self.current_frame = 0
+        self.state_changed = False
+
+    #
+    def set_animation_state(self, state: str):
+        valid_states = ['idle', 'run', 'crouch', 'jump', 'fall']
+        if state in valid_states:
+            self.state = state
+            self.current_frame = 0
+            # Change the number of sprites in the list when the state changes
+            # because the lists MAY have different lengths (hopefully not, w/e).
+            self.num_sprites = len(self.sprite_dictionary[self.state])
+            self.state_changed = True
+        else:
+            raise ValueError(f'State {state} is not a valid state!')
+
+    #
+    def get_initial_frame(self) -> pygame.Surface:
+        return self.sprite_dictionary[self.state][self.current_frame]
+
+    #
+    def on_animate(self) -> None:
+        if self.state_changed is False:
+            self.current_frame += 1
+            # Overstepped index, loop back to start of the animation.
+            if self.current_frame >= self.num_sprites:
+                self.current_frame = 0
+        self.entity.image = self.sprite_dictionary[self.state][self.current_frame]
+        self.state_changed = False
+
+    #
+    def update(self, animate: bool) -> None:
+        if animate is True:
+            self.on_animate()
---
+++ scripts/actor_components/animators/animator_interface.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+import pygame
+
+
+#
+class AnimatorInterface(ABC):
+
+    #
+    @abstractmethod
+    def get_initial_frame(self) -> pygame.Surface:
+        pass
+
+    #
+    @abstractmethod
+    def on_animate(self) -> None:
+        pass
+
+    #
+    @abstractmethod
+    def update(self, animate: bool) -> None:
+        pass

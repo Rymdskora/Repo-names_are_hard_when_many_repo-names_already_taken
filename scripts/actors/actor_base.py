6999a632eb27178e26d781a7a351930b9cad79a0
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-15 02:01:43 -0400

    Fixed group unpacking

--- scripts/actors/actor_base.py
+++ scripts/actors/actor_base.py
@@ -5,12 +5,12 @@ import pygame
 class ActorBase(pygame.sprite.Sprite):
 
     #
-    def __init__(self, animator, groups):
-        super().__init__(*groups)
+    def __init__(self, animator, position, *groups):
+        super().__init__(groups)
         self.animator = animator.set_entity(self)
 
         self.image = self.animator.get_initial_frame()
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_frect(center=position)
 
     #
     def update(self, delta_time, animate: bool):

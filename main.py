eb644eae35544584a8e15160dd8aecc12a7146d0
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-15 02:34:59 -0400

    Updated engine to use animator.

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from scripts.engine.engine_scene_manager import EngineSceneManager
+from scripts.engine.engine_animator import EngineAnimator
 from scripts.engine.engine_display import EngineDisplay
 import pygame
 import sys
 
 
 # Engine class, core of the game.
-class Engine:
+class EmberEngine:
 
     #
     def __init__(self):
         pygame.init()
         self.display = EngineDisplay(self)
+        self.animator = EngineAnimator(self)
         self.scene_manager = EngineSceneManager(self)
 
     # Run the game.
     def run(self) -> None:
         while True:
             events = pygame.event.get()
-            self.scene_manager.current_scene.update(events, self.display.display, False, self.display.delta_time)
+            self.animator.update()
+            self.scene_manager.current_scene.update(
+                events, self.display.display, self.animator.animate, self.display.delta_time)
             self.display.update()
+
 
     # Static because it should be callable wherever?
     @staticmethod
@@ -29,4 +34,4 @@ class Engine:
 
 # Create the engine and run it.
 if __name__ == '__main__':
-    Engine().run()
+    EmberEngine().run()

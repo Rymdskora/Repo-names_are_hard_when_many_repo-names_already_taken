233079c1a433cc676289f187aee192e9bc064d91
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-17 21:40:19 -0400

    Updated Actor to use Physicist.

--- scripts/actors/actor_base.py
+++ scripts/actors/actor_base.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pygame
 
 
@@ -5,13 +6,16 @@ import pygame
 class ActorBase(pygame.sprite.Sprite):
 
     #
-    def __init__(self, animator, position, *groups):
+    def __init__(self, animator, physicist, position: Tuple[int, int], *groups):
         super().__init__(groups)
         self.animator = animator.set_entity(self)
+        self.physicist = physicist.set_entity(self)
 
         self.image = self.animator.get_initial_frame()
         self.rect = self.image.get_frect(center=position)
+        self.direction = pygame.Vector2(0, 0)
 
     #
-    def update(self, delta_time, animate: bool):
+    def update(self, delta_time: float, animate: bool):
         self.animator.update(animate)
+        self.physicist.update(delta_time)

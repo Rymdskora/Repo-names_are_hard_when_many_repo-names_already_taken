eb71b79ec34ecfe98c20274604119f4ca69d16a7
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-25 18:09:19 -0400

    Updated to use orchestrator.

--- scripts/actors/actor_base.py
+++ scripts/actors/actor_base.py
@@ -6,14 +6,15 @@ import pygame
 class ActorBase(pygame.sprite.Sprite):
 
     #
-    def __init__(self, animator, physicist, position: Tuple[int, int], *groups):
+    def __init__(self, orchestrator, animator, physicist, position: Tuple[int, int], *groups):
         super().__init__(groups)
+        self.orchestrator = orchestrator.set_entity(self)
         self.animator = animator.set_entity(self)
         self.physicist = physicist.set_entity(self)
 
+        self.direction = pygame.Vector2(0, 0)
         self.image = self.animator.get_initial_frame()
         self.rect = self.image.get_frect(center=position)
-        self.direction = pygame.Vector2(0, 0)
 
     #
     def update(self, delta_time: float, animate: bool):

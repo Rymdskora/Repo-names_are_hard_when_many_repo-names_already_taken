b75e426df5c824316eaffec4c2fe649b6db71830
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-25 18:09:08 -0400

    Overhauled to deal with direction and states better.

--- scripts/actor_components/animators/animator_dictionary.py
+++ scripts/actor_components/animators/animator_dictionary.py
@@ -11,49 +11,45 @@ class AnimatorDict(ComponentBase, AnimatorInterface):
     def __init__(self, sprites: Dict[str, List[pygame.Surface]], state='idle'):
         super().__init__()
         self.sprite_dictionary = sprites
-        self.state = state
-        self.num_sprites = len(self.sprite_dictionary[self.state])
+        self.current_state = state
         self.current_frame = 0
-        self.state_changed = False
+        self.facing = 'right'
+        self.num_sprites = len(self.sprite_dictionary[self.current_state])
 
     #
-    def set_animation_state(self, state: str):
-        # If running in a specific direction, run needs to be flipped based on
-        # the direction the character is moving horizontally.
-        # I'm sure others will need to be flipped too.
-
-        valid_states = ['idle', 'run', 'crouch', 'jump', 'fall']
-        if state in valid_states:
-            self.state = state
-            self.current_frame = 0
-            # Change the number of sprites in the list when the state changes
-            # because the lists MAY have different lengths (hopefully not, w/e).
-            self.num_sprites = len(self.sprite_dictionary[self.state])
-            self.state_changed = True
-        else:
-            raise ValueError(f'State {state} is not a valid state!')
-
+    def set_state(self, state: str) -> None:
+        self.current_state = state
+        self.current_frame = 0
+        self.num_sprites = len(self.sprite_dictionary[self.current_state])
+            
     #
     def get_initial_frame(self) -> pygame.Surface:
-        return self.sprite_dictionary[self.state][self.current_frame]
+        return self.sprite_dictionary[self.current_state][self.current_frame]
 
     #
     def on_animate(self) -> None:
-        if self.state_changed is False:
-            self.current_frame += 1
-            # Overstepped index, loop back to start of the animation.
-            if self.current_frame >= self.num_sprites:
-                self.current_frame = 0
+        self.current_frame += 1
+        # Overstepped index, loop back to start of the animation.
+        if self.current_frame >= self.num_sprites:
+            self.current_frame = 0
 
-        if self.entity.direction.x == -1:
-            self.entity.image = self.get_flipped_frame()
+        self.should_reflect()
+
+        if self.facing == 'right':
+            self.entity.image = self.sprite_dictionary[self.current_state][self.current_frame]
         else:
-            self.entity.image = self.sprite_dictionary[self.state][self.current_frame]
-        self.state_changed = False
+            self.entity.image = self.get_reflected_frame()
+
+    #
+    def should_reflect(self):
+        if self.entity.direction.x == 1:
+            self.facing = 'right'
+        elif self.entity.direction.x == -1:
+            self.facing = 'left'
 
     # Only flips across the horizontal surface as I don't think I'll ever need a vertical flip!
-    def get_flipped_frame(self) -> pygame.Surface:
-        frame = self.sprite_dictionary[self.state][self.current_frame]
+    def get_reflected_frame(self) -> pygame.Surface:
+        frame = self.sprite_dictionary[self.current_state][self.current_frame]
         flipped_frame = pygame.transform.flip(frame, True, False)
         return flipped_frame
 

007f4ff401d155dfa9ec740a585d9a51d880026f
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-28 20:22:47 -0400

    Updated Actor to use Sentinel Components and Flags.

--- scripts/actors/actor_base.py
+++ scripts/actors/actor_base.py
@@ -1,3 +1,4 @@
+from data.definitions.flags import Flags
 from typing import Tuple
 import pygame
 
@@ -6,10 +7,11 @@ import pygame
 class ActorBase(pygame.sprite.Sprite):
 
     #
-    def __init__(self, animator, physicist, position: Tuple[int, int], states, *groups):
+    def __init__(self, animator, physicist, sentinel, position: Tuple[int, int], states, *groups):
         super().__init__(groups)
         self.animator = animator
         self.physicist = physicist
+        self.sentinel = sentinel
 
         self.image = self.animator.get_initial_frame()
         self.rect = self.image.get_frect(center=position)
@@ -18,6 +20,25 @@ class ActorBase(pygame.sprite.Sprite):
         self.states = states
         self.current_state = self.states[0]
 
+        # Hold a certain state because of condition(s)?
+        self.steady_state = False
+
+    #
+    def __repr__(self):
+        pass
+
+    #
+    def set_component_state(self, component: str, state: str) -> None:
+        state_contained = state in self.states
+        component_contained = hasattr(self, component)
+
+        if state_contained and component_contained:
+            self.__dict__[component].set_state(state)
+        elif not state_contained:
+            raise ValueError(f'State: "{state}" is not in {self.states}!')
+        elif not component_contained:
+            raise AttributeError(f'Component: "{component}" is not in {self.__dict__}!')
+
     #
     def set_component_states(self, state: str) -> None:
         if state in self.states:
@@ -25,12 +46,21 @@ class ActorBase(pygame.sprite.Sprite):
                 self.current_state = state
                 self.animator.set_state(state)
                 self.physicist.set_state(state)
+                self.sentinel.set_state(state)
             else:
                 pass
         else:
-            raise ValueError(f'State: {state} not in {self.states}!')
+            raise ValueError(f'State: "{state}" not in {self.states}!')
+
+    #
+    def manage_flags(self, flags: Flags) -> None:
+        if flags != 0:
+            if flags & Flags.EXHAUSTED:
+                pass
 
     #
     def update(self, delta_time: float, animate: bool):
         self.animator.update(self, animate)
         self.physicist.update(self, delta_time)
+        flags = self.sentinel.update(self)
+        self.manage_flags(flags)

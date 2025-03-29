e8446c4225febd3ac7a16d6791b1300df60b8eab
Jesse R.D. Behanna <JesseBehanna@protonmail.com>
2025-03-29 19:24:41 -0400

    Updated to support Flag management for Actors.

--- data/definitions/flags.py
+++ data/definitions/flags.py
@@ -7,19 +7,29 @@ class Flags:
 
     #
     @classmethod
-    def check_flag_existence(cls, flag):
+    def check_flag_existence(cls, flag) -> None:
         known_flags = [cls.EXHAUSTED]
         if flag not in known_flags:
             raise Exception(f'Flag {flag} does not exist!')
 
+    # No need to check flag existence here because
+    # they're already checked when a component raises them!
+    @classmethod
+    def manage_actor_flags(cls, actor, flags) -> None:
+        added_flags = flags & ~actor.flags  # New flags that aren't already set
+        removed_flags = actor.flags & ~flags  # Old flags that are no longer present
+
+        actor.flags |= added_flags  # Add new flags
+        actor.flags &= ~removed_flags  # Remove flags that are no longer active
+
     #
     @classmethod
-    def raise_component_flag(cls, component, flag):
+    def raise_component_flag(cls, component, flag) -> None:
         cls.check_flag_existence(flag)
         component.flags |= flag
 
     #
     @classmethod
-    def lower_component_flag(cls, component, flag):
+    def lower_component_flag(cls, component, flag) -> None:
         cls.check_flag_existence(flag)
         component.flags &= ~flag
